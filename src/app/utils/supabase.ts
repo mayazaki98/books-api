477b34feb388414657c7409cbf406a444908b1d0
mayazaki98 <n.yamamoto98@gmail.com>
2024-02-25 15:06:38 +0900

    supabaseサービスロールの環境変数名を変更

--- src/app/utils/supabase.ts
+++ src/app/utils/supabase.ts
@@ -10,7 +10,7 @@ export const supabase = createClient<Database>(
 
 export const supabaseAdmin = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE!
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE!
 );
 
 /**
